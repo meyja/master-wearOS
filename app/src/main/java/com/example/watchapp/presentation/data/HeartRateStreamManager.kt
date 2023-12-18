cffd2f299e03f1b9a447bd7a05f58dd493c0d4bf
Tobias Myren Foldal <tobiasmfoldal@gmail.com>
2023-12-18 12:36:03 +0100

    new location request, now active

--- app/src/main/java/com/example/watchapp/presentation/data/HeartRateStreamManager.kt
+++ app/src/main/java/com/example/watchapp/presentation/data/HeartRateStreamManager.kt
@@ -1,10 +1,12 @@
 package com.example.watchapp.presentation.data
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Context.LOCATION_SERVICE
 import android.content.pm.PackageManager
 import android.location.LocationManager
+import android.location.LocationRequest
 import android.util.Log
 import androidx.core.app.ActivityCompat
 import androidx.work.Constraints
@@ -15,6 +17,9 @@ import androidx.work.OneTimeWorkRequestBuilder
 import androidx.work.WorkManager
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
+import com.google.android.gms.tasks.CancellationToken
+import com.google.android.gms.tasks.CancellationTokenSource
+import com.google.android.gms.tasks.Tasks.await
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.launch
@@ -81,7 +86,7 @@ class HeartRateStreamManager(context: Context) {
                 sum += e.hr
                 Log.d(TAG, e.hr.toString())
             }
-            val loc = getLastLocation()
+            val loc = getCurrentLocation()
             Log.d(TAG, "doAnalysis: loc:${loc.toString()}")
             if (loc != null) {
                 val avg = sum / heartRateDataCopy.size
@@ -97,18 +102,14 @@ class HeartRateStreamManager(context: Context) {
 
     }
 
-    private fun getLastLocation(): Pair<String, String>? {
-        if (ActivityCompat.checkSelfPermission(
-                c,
-                Manifest.permission.ACCESS_FINE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
-                c,
-                Manifest.permission.ACCESS_COARSE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED
-        ) { // not permission
-            Log.d(TAG, "getLastLocation: not permission")
-            return null
-        }
+    /**
+     * Retrieves the last known location of the device.
+     *
+     * @return A Pair of strings containing the latitude and longitude of the location, or null if the location could not be retrieved.
+     */
+    @SuppressLint("MissingPermission")
+    private fun getLastLocation(): Pair<String, String>? { // currently not used
+        if (!hasPermission()) return null
 
         val lm = c.getSystemService(LOCATION_SERVICE) as LocationManager
         val location = lm.getLastKnownLocation(LocationManager.FUSED_PROVIDER)
@@ -126,6 +127,25 @@ class HeartRateStreamManager(context: Context) {
         return Pair(lat, long)
 
     }
+
+    /**
+     * Retrieves the current location of the device using the FusedLocationProviderClient.
+     * Active retrieval.
+     *
+     * @return A Pair of strings containing the latitude and longitude of the current location, or null if the location could not be retrieved.
+     */
+    @SuppressLint("MissingPermission")
+    fun getCurrentLocation(): Pair<String, String>? {
+
+        if (!hasPermission()) return null
+
+        val cancelToken = CancellationTokenSource()
+        val loc = await(fusedLocationProviderClient.getCurrentLocation(LocationRequest.QUALITY_HIGH_ACCURACY, cancelToken.token))
+
+        Log.d(TAG, "getCurrentLocation: ${loc.toString()}")
+        return Pair(loc.latitude.toString(), loc.altitude.toString())
+    }
+
 
     /**
      * Starts a worker to do a network request, sends lat, lon, and avg.
@@ -149,5 +169,20 @@ class HeartRateStreamManager(context: Context) {
 
         WorkManager.getInstance(c).enqueue(oneTimeWork)
 
+    }
+
+    /**
+     * Checking if the device has the correct permissions to do the location requests
+     *
+     * @return Boolean true if app has permission, false if not
+     */
+    private fun hasPermission(): Boolean {
+        return !(ActivityCompat.checkSelfPermission(
+            c,
+            Manifest.permission.ACCESS_FINE_LOCATION
+        ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+            c,
+            Manifest.permission.ACCESS_COARSE_LOCATION
+        ) != PackageManager.PERMISSION_GRANTED)
     }
 }
