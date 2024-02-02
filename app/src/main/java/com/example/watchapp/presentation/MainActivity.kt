21b3c27f8817528a1d8d0eac18a602b00583fa12
Tobias Myren Foldal <tobiasmfoldal@gmail.com>
2024-02-02 10:48:41 +0100

    Lagt til self report knapp

--- app/src/main/java/com/example/watchapp/presentation/MainActivity.kt
+++ app/src/main/java/com/example/watchapp/presentation/MainActivity.kt
@@ -7,6 +7,7 @@ import android.content.IntentFilter
 import android.os.Build
 import android.os.Bundle
 import android.os.PowerManager
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
@@ -15,6 +16,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -127,6 +129,12 @@ class MainActivity : ComponentActivity() {
                 onStop()
             }) {
                 Text("Stop", modifier = Modifier.padding(5.dp))
+            }
+            Spacer(modifier = Modifier.height(8.dp))
+            Button(modifier = Modifier.padding(horizontal = 16.dp).width(100.dp), onClick = {
+                Log.d("MainActivity", "MonitoringApp: Self Report clicked")
+            }) {
+                Text("Self Report", modifier = Modifier.padding(5.dp))
             }
         }
     }
